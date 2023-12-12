bfef4f1c29bc190956caacec9a9e507d0138f323
tommy martinez <thomasjohnmartinez@gmail.com>
2023-12-11 21:55:06 -0500

    slider ui

--- integrity.js
+++ integrity.js
@@ -3,42 +3,28 @@ let offset = 100;
 let el;
 let cover;
 
-function setup() {
-    canv = createCanvas(800, 200); 
-    canv.parent("controls");
-    // example values
-    colorKnob = new MakeKnobC("white", 50, ((width/4) * 0) + offset, 50, 0, 255, 0, 0,"", [0,0,255, 0], 0);
-    colorKnob2 = new MakeKnobC("white", 50, ((width/4) * 1) + offset, 50, 0, 255, 0, 0,"", [0,0,255, 0], 0);
-    colorKnob3 = new MakeKnobC("white", 50, ((width/4) * 2) + offset, 50, 0, 255, 0, 0,"", [0,0,25, 0], 0);
-    colorKnob4 = new MakeKnobC("white", 50, ((width/4) * 3) + offset, 50, 0, 255, 0, 0,"", [0,0,255,0], 0);
+let speedInput; 
+let speed;
+
+window.addEventListener("load", function() {
     el = document.getElementById("viddy");
-    cover = document.querySelector(".playBar");
-}
-  
+    cover = document.querySelector(".playButton");
+    speedInput = document.querySelector("#speed");
+    speedInput.oninput = function() {
+        el.playbackRate = this.value;
+      } 
+});
+
 function play() {
     el.play();
     cover.style.display = "none";
+    showControls();
 }
-function draw() {
-    background(0, 0, 255);
-    //background(colorKnob.knobValue); // Use the knob to control something
-    colorKnob.update();
-    colorKnob2.update();
-    colorKnob3.update();
-    colorKnob4.update();
+
+function showControls() {
+    setTimeout(() => {
+        document.querySelectorAll('.slidey').forEach(function(rangey) {
+            rangey.style.opacity = "1.0";
+        });
+      }, "4200");   
 }
-  
-function mousePressed() { 
-    colorKnob.active();
-    colorKnob2.active();
-    colorKnob3.active();
-    colorKnob4.active(); 
-}
-function mouseReleased() { 
-    colorKnob.inactive();
-    colorKnob2.inactive();
-    colorKnob3.inactive();
-    colorKnob4.inactive(); 
-}
-  
-  
